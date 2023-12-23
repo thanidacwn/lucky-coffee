f787ebc6a5864ebc6beae2b62fd448268955ca85
koonwill <93331101+koonwill@users.noreply.github.com>
2023-12-23 21:24:34 +0700

    add somework

--- backend/routes/itemRoutes.js
+++ backend/routes/itemRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.route('/all').get((req, res) => res.status(200).json({'message': 'get'}));
-router.route('/').post((req, res) => res.status(200).json('post'));
+router.route('/').post((req, res) => res.status(200).json(req.body));
 router.route('/').put((req, res) => res.status(200).json('put'));
 router.route('/').delete((req, res) => res.status(200).json('delete'));
 
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const mongoose = require('mongoose');
+const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 const port = 5000;
 
-app.use('/api', require('./routes/serverRouter'));
+app.use(express.json());
+app.use('/api', require('./routes/apiRouter'));
+app.use(errorHandler);
 
 app.get('/', (req, res) => res.status(200).json('Success'));
 
